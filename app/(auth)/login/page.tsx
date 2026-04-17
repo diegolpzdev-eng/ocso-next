6c1fab24a2879c44744211cf4c195879160e7c58
Luis Diego Hernandez Lopez <diegolpz.dev@gmail.com>
2026-04-17 13:54:01 -0600

    Mi primer commit backend

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { Input, Button } from "@nextui-org/react";
+
+export default function LoginPage() {
+
+  return (
+    <form className="bg-orange-500 px-10 py-2 rounded-md">
+      <p className="text-2xl my-4 text-white">
+        Iniciar sesión
+      </p>
+      <div className="flex flex-col gap-2 my-4 items-center">
+        <Input label="Email" name="userEmail" type="email" isRequired={true} size="sm" />
+        <Input label="Contraseña" name="userPassword" type="password" isRequired={true} size="sm" />
+      </div>
+      <div className="flex flex-col items-center gap-2">
+        <Button
+          color="primary"
+          type="submit"
+          >
+        
+        </Button>
+        <p className="text-white">
+          ¿No tienes cuenta? <Link href="/signup" className="text-red-600 underline">Registrate</Link>
+        </p>
+      </div>
+    </form>
+  );
+}
