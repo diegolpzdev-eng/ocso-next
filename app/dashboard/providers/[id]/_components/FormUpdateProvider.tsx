69730b73b6903a8f114d84ae5a5134dc6bc24f73
Luis Diego Hernandez Lopez <diegolpz.dev@gmail.com>
2026-04-27 23:37:00 -0600

    Actualizar provider

--- app/dashboard/providers/[id]/_components/FormUpdateProvider.tsx
+++ app/dashboard/providers/[id]/_components/FormUpdateProvider.tsx
@@ -10,9 +10,13 @@ export default function FormUpdateProvider ({provider} : {provider: Provider}) {
   return (
     <>
     <form action={updateProviderWithId} className="flex flex-wrap gap-4 flex-grow-0 bg-orange-200 rounded-md px-10 py-10 mr-20 items-center justify-center">
+
       <Input className="max-w-[250px]" defaultValue={provider.providerName} label="Nombre" placeholder="Pecsi" name="providerName"/>
+
       <Input className="max-w-[250px]" defaultValue={provider.providerEmail} label="Correo" placeholder="business@pecsi.com" name="providerEmail"/>
+
       <Input className="max-w-[250px]" defaultValue={provider.providerPhoneNumber} label="Número" placeholder="444XXXXX" name="providerPhoneNumber"/>
+      
       <Button color="primary" type="submit"> Actualizar </Button>
       <DeleteProvider>
           <h1 className="text-white text-4xl text-center">¿Estás seguro de eliminar al proveedor <b>{provider.providerName}</b>?</h1>
