5d9d1e93385623070474d7fd312fb2c48c19b77c
Luis Diego Hernandez Lopez <diegolpz.dev@gmail.com>
2026-04-27 23:28:30 -0600

    feat: Crear provedor e implementar

--- app/dashboard/providers/_components/CreateProvider.tsx
+++ app/dashboard/providers/_components/CreateProvider.tsx
@@ -8,9 +8,12 @@ export default function CreateProvider({children}: {children: ReactNode}) {
 
   return (
     <div>
+
       <Button onPress={onOpen} color="primary"><LuPlus size="20"/></Button>
+
       <Modal className="bg-orange-400" isOpen={isOpen} onOpenChange={onOpenChange}>
         <ModalContent className="w-full">
+          
           {() => (
             <>
               <ModalBody>
